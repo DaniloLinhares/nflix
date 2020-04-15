9ace58fab7d059a03cf8acdd6f18f9b0413a3db6
Danilo <daniloferreira2004@gmail.com>
2020-04-15 16:03:02 -0300

    subindo aquivos nflix

---
+++ features/step_definitions/movie_step.rb
@@ -0,0 +1,13 @@
+Dado("que {string} é um novo filme") do |movie_code|
+file = YAML.load_file(File,join(Dir.pwd, "feature/support/fixtures/movies.yaml"))
+@movie = file[movie_code]
+
+end
+
+Quando("eu faço o cadstro deste filme") do
+
+end
+
+Então("devo ver o novo filme na lista") do
+
+end
