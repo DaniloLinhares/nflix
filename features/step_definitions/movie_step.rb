7a21f2529468d7449cc22eceb72e38d28afb72a4
Danilo <daniloferreira2004@gmail.com>
2020-04-26 00:35:17 -0300

    retirando espaço

--- features/step_definitions/movie_step.rb
+++ features/step_definitions/movie_step.rb
@@ -1,7 +1,6 @@
 Dado("que {string} é um novo filme") do |movie_code|
 file = YAML.load_file(File,join(Dir.pwd, "feature/support/fixtures/movies.yaml"))
 @movie = file[movie_code]
-
 end
 
 Quando("eu faço o cadstro deste filme") do
