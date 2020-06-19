619da1059ed7e4ffea296a3b6b4aa39dc80a8096
Danilo <daniloferreira2004@gmail.com>
2020-06-19 15:48:56 -0300

    configurando multbrowsers

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,8 +9,19 @@ World(Helpers)
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
+case ENV["BROWSER"]
+when "firefor"
+    @driver = :selenium
+when "chrome"
+    @driver = :selenium_chrome
+when "headless"
+    @driver = :selenium_chrome_headless
+else
+    puts "Invalid Browser"
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium
+    config.default_driver = @driver
     config.app_host = CONFIG["url"]
     config.default_max_wait_time = 10
 end
